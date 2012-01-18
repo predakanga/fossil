7acea04ab5e20730cc53df786b530b3ed98703ea
predakanga <predatory.kangaroo@gmail.com>
2012-01-19 01:15:38 +1100

    Added extension to BBCode when Users plugin is in use, [user=predakanga]

---
+++ src/Plugins/Users/Util/HTML_BBCodeParser_Filter_FossilUser.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * Copyright (c) 2011, predakanga
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+use Fossil\Plugins\Users\Models\User;
+
+/**
+ * Description of HTML_BBCodeParser_Filter_FossilUser
+ *
+ * @author predakanga
+ */
+class HTML_BBCodeParser_Filter_FossilUser extends \HTML_BBCodeParser_Filter {
+    /**
+     * @var Fossil\ObjectContainer
+     */
+    public $container = null;
+    
+    public $_definedTags = array(
+        'user' => array(
+            'htmlopen'  => 'a',
+            'htmlclose' => 'a',
+            'allowed'   => 'none',
+            'attributes'=> array('url' => 'href=%2$s%1$s%2$s')
+        )
+    );
+    
+    public function _preparse() {
+        $this->_preparsed = $this->_text;
+        $self = $this;
+        
+        $this->_preparsed = preg_replace_callback('/\[user=([^\]]+)\]/', function($matches) use($self) {
+            // Look up the username from $matches[1]
+            // Return string of form...
+            // [user url=?controller=user&action=view&id=1]admin[/user]
+            $user = User::findOneByName($self->container, $matches[1]);
+            if($user) {
+                return "[user url=\"?controller=user&action=view&id={$user->id}\"]{$user->name}[/user]";
+            } else {
+                return $matches[0];
+            }
+        }, $this->_text);
+    }
+}
+
+?>
